13ce3b1ff953d99060bc47c3691e44cab538e620
Lakshay Kumar <lakshay.gla_cs21@gla.ac.in>
2025-03-03 23:56:24 +0530

    LAKSHAY

---
+++ src/main/java/com/example/GreetingApp/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package com.example.GreetingApp.controller;
+
+import com.example.GreetingApp.service.GreetingService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    private final GreetingService greetingService;
+
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @GetMapping("/message")
+    public String getMessage(@RequestParam(required = false) String firstName,
+                             @RequestParam(required = false) String lastName) {
+        return greetingService.getGreetingMessage(firstName, lastName);
+    }
+}
