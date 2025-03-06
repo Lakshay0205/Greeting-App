768ffc22c1f1ccbd4ba062a0eccc52db58f4204e
Lakshay Kumar <lakshay.gla_cs21@gla.ac.in>
2025-03-06 10:51:55 +0530

    LAKSHAY

---
+++ src/main/java/com/example/GreetingApp/controller/AuthController.java
@@ -0,0 +1,22 @@
+package com.example.GreetingApp.controller;
+
+import com.example.GreetingApp.model.User;
+import com.example.GreetingApp.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody User user) {
+        return ResponseEntity.ok(userService.registerUser(user));
+    }
+}
